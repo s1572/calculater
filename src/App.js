b7509f5c0cbe830be083d2290117fc994774e9af
s1572 <shubhamatrish21@gmail.com>
2021-02-27 22:14:26 +0530

    mycalculator

---
+++ src/App.js
@@ -0,0 +1,82 @@
+
+import './App.css';
+import React,{useState,useEffect,useRef} from 'react';
+
+function App() {
+  const [result,setResult] = useState("");
+  const inputRef =useRef(null);
+
+
+  useEffect(()=> inputRef.current.focus());
+
+  function handleclick(e){
+
+    setResult( result + e.target.name);
+  }
+
+  function backspace(){
+    setResult(result.slice(0,result.length-1));
+  }
+
+  function clear(){
+    setResult("");
+  }
+
+  function calculate(){
+    try{
+      setResult(eval(result).toString());
+    } catch(error){
+        setResult("Error");
+      }
+    }
+
+
+
+  return (
+
+<div className="bg" >
+
+    <div className="calc-app" > 
+
+      <h1 className="heading"> Calculator
+     </h1>
+      <form >
+        <input type="text" value={result}  ref={inputRef} />
+      </form> 
+      
+      <div className="keypad">
+        <button id="clear" onClick={clear}>Clear</button>
+        <button id="backspace" onClick={backspace}>c</button>
+        
+           <button  id="clr" name = "+" onClick={handleclick}>+</button> 
+
+        <button name = "7"  onClick={handleclick}>7</button>
+        <button name = "8" onClick={handleclick}>8</button>
+        <button name = "9"  onClick={handleclick}>9</button>
+        <button id="clr" name = "-" onClick={handleclick}>-</button>
+        <button name = "4" onClick={handleclick}>4</button>
+        <button  name = "5" onClick={handleclick}>5</button>
+        <button  name = "6" onClick={handleclick}>6</button>
+        <button  id="clr" name = "*"  onClick={handleclick}>&times;</button>
+        <button name = "1" onClick={handleclick}>1</button>
+        <button name = "2" onClick={handleclick}>2</button>
+        
+        <button name = "3" onClick={handleclick}>3</button>
+        <button  id="clr" name = "/" onClick={handleclick}>/</button>
+        <button name = "0" onClick={handleclick}>0</button>
+        <button  name = "."  onClick={handleclick}>.</button>
+        
+
+        <button id="result" onClick={calculate}>Result</button>
+
+      </div>
+
+
+      
+      
+    </div>
+    </div>
+  );
+}
+
+export default App;
